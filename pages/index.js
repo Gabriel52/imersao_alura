35d518ba627d668050c10272bdc0a6e9c4e86816
Gabriel52 <gabrielbrune52@gmail.com>
2021-01-28 10:48:18 -0300

    second day

--- pages/index.js
+++ pages/index.js
@@ -69,6 +69,7 @@ export default function Home() {
       <QuizBackground backgroundImage={config.bg}>
         <Head>
           <title>Alura Quiz</title>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
 
         </Head>
         <QuizContainer>
