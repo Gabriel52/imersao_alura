803917f1c2e4c2f41b033133409fb4b73ba658bb
Gabriel52 <gabrielbrune52@gmail.com>
2021-01-29 21:02:50 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -9,55 +9,15 @@ import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+import QuizLogo from '../src/components/QuizLogo';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button'
 
-/* eslint-disable */
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `;
-// const BackgroundImage = styled.div`
-//   background-image: url(${config.bg});
-//   flex: 1;
-//   background-size: cover;
-// `
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`
-export const Button = styled.button`
-    margin-top: 4%;
-    border-radius: 4px;
-    padding: 8px;
-    border: solid 2px #00e676;
-    margin: 10px;
-    line-height: 24px;
-    border-radius: 10px;
-    position: relative;
-    box-sizing: border-box;
-    cursor: pointer;
-    transition: all 400ms ease;
-    &:before {
-      content: '';
-      position: absolute;
-      top: 0px;
-      left: 0px;
-      width: 0px;
-      height: 43px;
-      background: rgb(153, 51, 153, 0.2);
-      border-radius: 10px;
-      transition: all 2s ease;
-    }
-    &:hover&:before{
-      width: 100%;
-    
-    }
-`
 
 
 export default function Home() {
@@ -73,6 +33,7 @@ export default function Home() {
 
         </Head>
         <QuizContainer>
+          <QuizLogo/>
           <Widget>
             <Widget.Header>
               <h1>The legend of zelda</h1>
@@ -86,12 +47,7 @@ export default function Home() {
                 console.log("oie")
 
               }}> 
-                <input className="form-control" onChange={function(evt){
-                  
-                  console.log(evt.target.value)
-                  setName(evt.target.value)
-                
-                }} placeholder="Diz ai seu nome" /><br/>
+                <Input  onChange={(evt) => setName(evt.target.value)} placeholder="Diz ai seu nome" /><br/>
                 <Button type="submit" disabled={name.length === 0}>
                   Vamos jogar {name}
                 </Button>
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,148 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable*/
 import React from 'react';
+import config from '../config.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+import Button from '../src/components/Button';
 
-export default function QuizPage() {
+
+
+
+function LoadingWidget() {
   return (
-    <>
-      <h1>Pagina Quiz</h1>
-    </>
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
+
+      <Widget.Content>
+        Pagina em carregamento
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+
+function QuestionWidget({ questionIndex, question, totalQuestions, onSubmit }){
+  const questionId = `question__${questionIndex}`
+  return(
+    <Widget>
+      <Widget.Header>
+        <h3>
+          {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+        </h3>
+      </Widget.Header>
+
+      <img
+        alt="Descrição"
+        style={{
+          width:'100%',
+          height:'150px',
+          objectFit:'cover'
+        }}
+        src="https://www.oficinadanet.com.br/imagens/post/15974/gif_1.gif"
+      />
+      <Widget.Content>
+        <h2>
+          {question.title}
+        </h2>
+        <p>
+          {question.description}
+        </p>
+
+        <form 
+          onSubmit={(infodoEvento)=>{
+            infodoEvento.preventDefault()
+            onSubmit()
+          }}
+        >
+          {question.alternatives.map((alternative, index, alternativeIndex)=>{
+            console.log("Pare de reclamar")
+            const alternativeId = `alternative__${alternativeIndex}`
+            return (
+              <Widget.Topic
+                as="label"
+                htmlFor={alternativeId}
+              >
+                <input
+                  //  style={display: "none"}
+                  id={alternativeId}
+                  name={questionId}
+                  type="radio"
+                />              
+              {alternative}
+              </Widget.Topic>
+            )
+          })}
+
+  {/*         
+          Serve para debugar o codigo
+          <pre>
+            {JSON.stringify(question, null, 4)}
+          </pre> */}
+
+          <Button type="submit">
+            Confirmar
+          </Button>
+        </form>
+      </Widget.Content>
+    </Widget>
+  )
+}
+
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT',
+};
+export default function QuizPage() {
+  const [screenState, setScreenState] = React.useState(screenStates.LOADING);
+  const totalQuestions = config.questions.length;
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
+  const questionIndex = currentQuestion;
+  const question = config.questions[questionIndex];
+
+  // [React chama de: Efeitos || Effects]
+  // React.useEffect
+  // atualizado === willUpdate
+  // morre === willUnmount
+  React.useEffect(() => {
+    // fetch() ...
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1 * 1000);
+  // nasce === didMount
+  }, []);
+
+  function handleSubmitQuiz() {
+    const nextQuestion = questionIndex + 1;
+    if (nextQuestion < totalQuestions) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setScreenState(screenStates.RESULT);
+    }
+  }
+
+  return (
+    <QuizBackground backgroundImage={config.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        {screenState === screenStates.QUIZ && (
+          <QuestionWidget
+            question={question}
+            questionIndex={questionIndex}
+            totalQuestions={totalQuestions}
+            onSubmit={handleSubmitQuiz}
+          />
+        )}
+
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+
+        {screenState === screenStates.RESULT && <div>Você acertou X questões, parabéns!</div>}
+      </QuizContainer>
+    </QuizBackground>
   );
 }
